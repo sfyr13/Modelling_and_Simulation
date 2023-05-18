d0510b5490f8c04da40286eae3c3cce02d4ad7a0
Μάνος Σφυράκης <75785904+sfyr13@users.noreply.github.com>
2023-05-18 11:00:58 +0300

    Add files via upload

---
+++ 2.Sim_Mod_Sfyrakis_Emmanouil_9507_assignment_2/θέμα_1/simulation_2.m
@@ -0,0 +1,89 @@
+function[t_set] = simulation_2(g, am)
+
+a = 1.5;
+b = 2;
+u = 3*cos(2*t);
+
+x0(1:6)=0;
+
+t_span = 0:0.01:30;
+N = length(t_span);
+
+[~, x ] = ode45(@(t, x) myfcn2(t, x, u, am, g, a, b), t_span, x0);
+
+output = x(:, 1);
+theta1_hat = x(:, 2);
+theta2_hat = x(:, 3);
+output_hat = x(:, 6);
+
+out_dif = zeros(N, 1);
+
+for i = 1:N
+    out_dif(i) = output(i) - output_hat(i);
+end
+    
+
+a_hat = zeros(N, 1);
+
+for i = 1: N
+    a_hat( i ) = am - theta1_hat( i );
+end
+
+b_hat = theta2_hat;
+
+sq_err = zeros(N, 1);
+
+for i = 1:N
+    sq_err(i) = (output(i) - output_hat(i))^2;
+end
+
+% squared error
+sq_err = zeros( N, 1 );
+for i = 1: N
+    sq_err( i ) = (output( i ) - output_hat( i ) )^2;
+end
+    
+% percentage error
+perc_err = zeros( N, 1 );
+for i = 1: N
+    perc_err( i ) = abs((output( i ) - output_hat( i ) ) /  output( i ) );
+end
+ 
+% settling time
+t_set = -1;
+for i = 1: N
+    if t_set < 0 && perc_err( i ) < 0.05
+        t_set = t_span( i );
+    end
+    if perc_err( i ) >= 0.05
+        t_set = -1;
+    end
+end
+
+
+%plots
+
+figure(1);
+hold on;
+plot(t_span, output);
+plot(t_span, output_hat);
+hold off;
+xlabel('time(s)');
+title('Real and estimated output');
+legend('$output$', '$\hat{output}$','interpreter', 'latex');
+
+figure(2);
+plot(t_span, out_dif);
+xlabel('time(s)');
+title('Difference between real and estimated output');
+
+figure(3);
+hold on;
+plot(t_span, a_hat);
+plot(t_span, b_hat);
+hold off;
+xlabel('Time(s)');
+title('Estimated Parameters');
+legend('$\hat{a}$', '$\hat{b}$','interpreter', 'latex');
+
+end
---
+++ 2.Sim_Mod_Sfyrakis_Emmanouil_9507_assignment_2/θέμα_1/trials.m
@@ -0,0 +1,10 @@
+dt = [];
+for g = 1: 1: 50
+    for am = 1: 0.5: 8
+        dt = [dt; g, am, simulation_1(g, am)];
+    end
+end
+
+[min, idx] = min(dt(:, 3));
+g = dt(idx, 1)
+am = dt(idx, 2)
